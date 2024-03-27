c8b0d75ac586a062e59b64325208c459628d6962
Amal Anilkumar <amalanilkumar282@gmail.com>
2024-03-27 10:23:00 +0530

    poppins.variable error in root-layout modified with original layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,24 @@
 import Footer from "@/components/ui/shared/Footer";
 import Header from "@/components/ui/shared/Header";
 
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({ 
+  subsets: ["latin"],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-poppins', 
+})
+
+export const metadata: Metadata = {
+  title: "Collab-E",
+  description: "Web application to coordinate events exclusively created for KTU students.",
+  icons:{
+    icon:'/assets/images/logo.svg',
+  }
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -9,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
         <Header />
-      <body className={Poppins.variable}>{children}</body>
+      <body className={poppins.variable}>{children}</body>
         <Footer />
     </html>
   );
